31129b6f6a9d2f0133a3f60c7b4561a77f75467c
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2014-05-20 19:52:06 +0900

    Update section 1.3

--- tspl3/section-1.3.scm
+++ tspl3/section-1.3.scm
@@ -1,6 +1,8 @@
 ;; Section 1.3. Typographical and Notational Convensions
 ;; http://www.scheme.com/tspl3/start.html#./start:h3
 
+;; Scheme expressions are usually evaluated to single value.
+
 ;; procedure can also return multiple values
 (define two-values
   (lambda ()
@@ -11,12 +13,9 @@
 (format #t "~a~%" (values-ref (values 1 2 3) 1)) ; => 2
 (format #t "~a~%" (call-with-values two-values list)) ; => (1 2 3)
 
-;; condition
-;; http://www.gnu.org/software/mit-scheme/documentation/mit-scheme-ref/Condition-Types.html
-; (format #t "~a%" (assertion-condition? 1))
-
 ;; vector-set
 (define my-vector
   #(my vector-value))
+
 (format #t "~a~%" (vector-set! my-vector 1 #\a)) ; => #<undef>
 (format #t "~a~%" my-vector) ; => #(my a)
