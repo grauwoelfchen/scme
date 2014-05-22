44635c2f12b1a3dfbe403b7a7b045343c8ab04bc
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2014-05-23 01:44:43 +0900

    Add exercises of section 2.2

--- tspl3/section-2.2.scm
+++ tspl3/section-2.2.scm
@@ -79,7 +79,8 @@
 ;; * cons is used as verb.
 ;; * A list is sequence of pair.
 ;; * Each pair's cdr is the next pair in sequence.
-;; * cdr of the last pair in proper list is empty list '(). Otherwise  the Sequen of pair's form is improper.
+;; * cdr of the last pair in proper list is empty list '().
+;;   Otherwise  the Sequen of pair's form is improper.
 ;; * The empty list is proper list.
 ;; * pair that it's cdr is proper list is a proper list.
 
@@ -93,7 +94,7 @@
 ;; Exercise
 
 ;; ex. 2.2.1
-
+(format #t "~%;; ex. 2.2.1. ~%~%")
 ; a
 (format #t "~a~%" (+ -8.7 (* 1.2 (- 2 1/3)))) ; => -6.699999999999999
 ; b
@@ -104,23 +105,55 @@
 (format #t "~a~%" (* (* (* (* (* (* 1 -2) 3) -4) 5) -6) 7)) ; -5040
 
 ;; ex. 2.2.2
-; (format #t "~a~%" (+ #(my-vector) '()))
-; => operation + is not defined between #(my-vector) and ()
-; (format #t "~a~%" (+ 0 '()))
-; => operation + is not defined between () and ()
-; (format #t "~a~%" (+ '() '()))
-; => operation + is not defined between 0 and ()
-; (format #t "~a~%" (+ "3" 0))
-; => operation + is not defined between 0 and "3"
-; (format #t "~a~%" (- 0 "1010")) ; => 0
-; => operation - is not defined between 0 and "1010"
-; (format #t "~a~%" (- #t #f))
-; (format #t "~a~%" (- '(1 2 3) '(3)))
-; => operation - is not defined between (1 2 3) and (3)
-; => operation - is not defined between #t and #f
+(format #t "~%;; ex. 2.2.2. ~%~%")
+
+;;; when decimal is given, expression will return decimal type.
+(format #t "~a~%" (+ 1.0 1)) ; => 2.0
+(format #t "~a~%" (+ 4 2.0)) ; => 2.0
+(format #t "~a~%" (- 2.0 2/4)) ; => 1.5
+(format #t "~a~%" (- 2/4 2.0)) ; => 2.5
+(format #t "~a~%" (* 2.0 1/3)) ; => 0.6666666666666666
+(format #t "~a~%" (/ 8.0 1/2)) ; => 16.0
+;;; when fraction is given, expression will return fraction in most case. but if possible, it will be integer.
+(format #t "~a~%" (+ 1/2 1/2)) ; => 1
+(format #t "~a~%" (+ 4.0 1/2)) ; => 4.5
+(format #t "~a~%" (- 1/2 -3)) ; => 7/2
+(format #t "~a~%" (- 1/2 3)) ; => -5/2
+;;; when integer is given, expression will return integer is most case. but if impossible, it will be fracton.
+(format #t "~a~%" (/ 20 13)) ; => 20/13
+(format #t "~a~%" (- 1 20)) ; => -19
+;;; zero
 (format #t "~a~%" (/ 3 0)) ; => +inf.0
 (format #t "~a~%" (/ 0 3)) ; => 0
-; (format #t "~a~%" (/ "0" 3))
-; => operation / is not defined between "0" and 3
-; (format #t "~a~%" (* + -))
-; operation * is not defined between #<subr +> and #<subr ->
+;; priority: decimal > fraction > integer
+
+;; ex. 2.2.3.
+(format #t "~%;; ex. 2.2.3. ~%~%")
+; a
+(format #t "~a~%" (cons 'car 'cdr)) ; => (car . cdr)
+; b
+(format #t "~a~%" (list 'this '(is silly))) ; => (this (is silly))
+; c
+(format #t "~a~%" (cons 'is '(this silly?))) ; => (is this silly?)
+; d
+(format #t "~a~%" (quote (+ 2 3))) ; => (+ 2 3)
+; e
+(format #t "~a~%" (cons '+ '(2 3))) ; => (+ 2 3)
+; f
+(format #t "~a~%" (car '(+ 2 3))) ; => +
+; g
+(format #t "~a~%" (cdr '(+ 2 3))) ; => '(2 3)
+; h
+(format #t "~a~%" cons) ; => #<subr cons>
+; i
+(format #t "~a~%" (quote cons)) ; => cons
+; j
+(format #t "~a~%" (quote (quote cons))) ; => 'cons
+; k
+(format #t "~a~%" (car (quote (quote cons)))) ; => quote
+; l
+(format #t "~a~%" (+ 2 3)) ; => 5
+; m
+(format #t "~a~%" (+ '2 '3)) ; => 5
+; n
+(format #t "~a~%" (+ (car '(2 3)) (car (cdr '(2 3))))) ; => 5
